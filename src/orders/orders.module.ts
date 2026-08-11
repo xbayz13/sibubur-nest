fcda35e15a5bd9d252544ad43f63c1211b164256
Ikhsan Pahdian <ikhsanpahdian@Ikhsans-MacBook-Air.local>
2026-08-11 23:37:35 +0700

    fix(orders): register ProductAddonProduct repo in OrdersModule (boot crash)

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -4,6 +4,7 @@ import { Order } from '../entities/order.entity';
 import { OrderItem } from '../entities/order-item.entity';
 import { OrderItemAddon } from '../entities/order-item-addon.entity';
 import { Product } from '../entities/product.entity';
+import { ProductAddonProduct } from '../entities/product-addon-product.entity';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { OrderEventsListener } from './order-events.listener';
@@ -11,7 +12,13 @@ import { GuardsModule } from '../common/guards/guards.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Order, OrderItem, OrderItemAddon, Product]),
+    TypeOrmModule.forFeature([
+      Order,
+      OrderItem,
+      OrderItemAddon,
+      Product,
+      ProductAddonProduct,
+    ]),
     GuardsModule,
   ],
   providers: [OrdersService, OrderEventsListener],
